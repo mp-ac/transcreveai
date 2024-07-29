4c3dfe72154979c670a50202d4e6f546942fe648
Antonio Anerão <anerao.junior@gmail.com>
2024-07-29 15:04:56 -0500

    add: converter arquivos para .mp3 caso nao seja mp3, ogg ou wav; tempo de execucao do script para converter

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from flask import Flask, request, jsonify, render_template, redirect, url_for, send_from_directory
+from flask import Flask, request, jsonify, redirect, url_for, send_from_directory
 import os
 from dotenv import load_dotenv
 from transcribe import transcrever_audio
 from pydub import AudioSegment
+import time
 
 load_dotenv('./.env')
-
-#local do ffmpeg
 AudioSegment.ffmpeg = "/usr/bin/ffmpeg"
 
 app = Flask(__name__)
@@ -18,35 +17,61 @@ if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route('/', methods=['GET'])
 def index():
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            return redirect(request.url)
-        file = request.files['file']
-        if file.filename == '':
-            return redirect(request.url)
-        if file:
-            file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-            file.save(file_path)
-            caminho_transcricao = transcrever_audio(file.filename)
-            transcricao_filename = os.path.basename(caminho_transcricao)
-            return jsonify(
-                    {
-                        "download":  os.getenv('URL')+":"+os.getenv('PORT')+"/"+url_for('download_file', filename=transcricao_filename),
-                    }
-            )
     return jsonify(
             {
-                "status": "ok",
+                "status": "success",
+                "app name": os.getenv('APP_NAME'),
                 "message": "Transcrição de áudio com Whisper v3 LARGE",
                 "versão": "v0.0.1"
             }
     )
 
+
+@app.route('/transcrever', methods=['POST'])
+def transcrever():
+    tempo_inicio = time.time()
+    if 'file' not in request.files:
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        return redirect(request.url)
+    if file:
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(file_path)
+
+        _, file_extension = os.path.splitext(file.filename)
+        if file_extension.lower() not in ['.mp3', '.ogg', '.wav']:
+            print("Converter arquivo para .mp3")
+
+            # Converter para .mp3
+            audio = AudioSegment.from_file(file_path)
+            mp3_filename = os.path.splitext(file.filename)[0] + '.mp3'
+            mp3_path = os.path.join(app.config['UPLOAD_FOLDER'], mp3_filename)
+            audio.export(mp3_path, format="mp3")
+            caminho_transcricao = transcrever_audio(mp3_filename)
+
+        caminho_transcricao = transcrever_audio(file.filename)
+        transcricao_filename = os.path.basename(caminho_transcricao)
+
+        tempo_fim = time.time()
+        tempo_total = tempo_fim - tempo_inicio
+        tempo_total = time.strftime("%H:%M:%S", time.gmtime(tempo_total))
+
+        return jsonify(
+                {
+                    "download":  os.getenv('URL')+":"+os.getenv('PORT')+url_for('download', filename=transcricao_filename),
+                    "tempo_execucao": tempo_total,
+                }
+        )
+
+
 @app.route('/download/<filename>')
-def download_file(filename):
+def download(filename):
     return send_from_directory(TRANSCRIBE_FOLDER, filename, as_attachment=True)
+
 
 if __name__ == '__main__':
     app.run(host=os.getenv('URL'), port=os.getenv('PORT'), debug=True)
