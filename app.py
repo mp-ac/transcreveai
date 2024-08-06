6474f6169669bfae8c51d330f4b08e8ccbaa82f1
Antonio Anerão <anerao.junior@gmail.com>
2024-08-06 11:35:45 -0500

    chore: loggin transformado em uma funcao

--- app.py
+++ app.py
@@ -13,11 +13,15 @@ AudioSegment.ffmpeg = "/usr/bin/ffmpeg"
 
 app = Flask(__name__)
 
-logging.basicConfig(
-    filename='transcricao.log',
-    level=logging.INFO,
-    format='%(asctime)s - %(message)s'
-)
+
+def registrar_log(arquivo_log, mensagem):
+    logging.basicConfig(
+        filename=arquivo_log,
+        level=logging.INFO,
+        format='%(asctime)s - %(message)s'
+    )
+    logging.info(mensagem)
+
 
 # Configura o diretório para uploads de áudio
 UPLOAD_FOLDER = 'audios'
@@ -62,62 +66,65 @@ def transcrever():
         timestamps = timestamps_str == 'true'  # Converte para booleano
     else:
         timestamps = timestamps_str == 'false'  # Converte para booleano
-    # else:
-    #     return jsonify(
-    #         {
-    #             "status": "error",
-    #             "message": "Parâmetro 'timestamps' deve ser 'true' ou 'false'"
-    #         }
-    #     )
-    #     exit()
 
     if 'file' not in request.files:
         return redirect(request.url)
     file = request.files['file']
     if file.filename == '':
         return redirect(request.url)
-    if file:
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-        file.save(file_path)
-        print("Upload concluído")
 
-        tempo_inicio = time.time()
-        _, file_extension = os.path.splitext(file.filename)
-        if file_extension.lower() not in ['.mp3', '.ogg', '.wav']:
-            print("Converter arquivo para .mp3")
+    try:
+        if file:
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+            file.save(file_path)
+            print("Upload concluído")
 
-            # Converter para .mp3
-            audio = AudioSegment.from_file(file_path)
-            mp3_filename = os.path.splitext(file.filename)[0] + '.mp3'
-            mp3_path = os.path.join(app.config['UPLOAD_FOLDER'], mp3_filename)
-            audio.export(mp3_path, format="mp3")
-            print("Arquivo convertido pra .mp3. Iniciando transcrição")
-            caminho_transcricao = transcribe.transcrever_audio(mp3_filename, timestamp=timestamps)
+            tempo_inicio = time.time()
+            _, file_extension = os.path.splitext(file.filename)
+            if file_extension.lower() not in ['.mp3', '.ogg', '.wav']:
+                print("Converter arquivo para .mp3")
 
-        else:
-            print("Iniciando transcrição")
-            caminho_transcricao = transcribe.transcrever_audio(file.filename, timestamp=timestamps)
+                # Converter para .mp3
+                audio = AudioSegment.from_file(file_path)
+                mp3_filename = os.path.splitext(file.filename)[0] + '.mp3'
+                mp3_path = os.path.join(app.config['UPLOAD_FOLDER'], mp3_filename)
+                audio.export(mp3_path, format="mp3")
+                print("Arquivo convertido pra .mp3. Iniciando transcrição")
+                caminho_transcricao = transcribe.transcrever_audio(mp3_filename, timestamp=timestamps)
 
-        transcricao_filename = os.path.basename(caminho_transcricao)
+            else:
+                print("Iniciando transcrição")
+                caminho_transcricao = transcribe.transcrever_audio(file.filename, timestamp=timestamps)
 
-        tempo_fim = time.time()
-        tempo_total = tempo_fim - tempo_inicio
-        tempo_total = time.strftime("%H:%M:%S", time.gmtime(tempo_total))
+            transcricao_filename = os.path.basename(caminho_transcricao)
 
-        # Retorne sempre um JSON, pois o HTML será manipulado pelo JavaScript
-        download_link = url_for('download', filename=transcricao_filename)
+            tempo_fim = time.time()
+            tempo_total = tempo_fim - tempo_inicio
+            tempo_total = time.strftime("%H:%M:%S", time.gmtime(tempo_total))
 
-        os.remove(file_path)
-        logging.info(
-            f"Arquivo '{file.filename}' - Tempo execução: {tempo_total}. - " +
-            f"Salvo como '{transcricao_filename}'."
-        )
+            # Retorne sempre um JSON, pois o HTML será manipulado pelo JavaScript
+            download_link = url_for('download', filename=transcricao_filename)
 
+            os.remove(file_path)
+            registrar_log(
+                'transcricao.log',
+                f"Arquivo '{file.filename}' - Tempo execução: {tempo_total}. - " +
+                f"Salvo como '{transcricao_filename}'."
+            )
+
+            return jsonify(
+                {
+                    "status": "success",
+                    "download":  "http://"+os.getenv('URL_PUB')+":"+os.getenv('PORT')+download_link,
+                    "tempo_transcricao": tempo_total,
+                }
+            )
+    except Exception as e:
+        registrar_log('error.log', f"Erro: {e}")
         return jsonify(
             {
-                "status": "success",
-                "download":  "http://"+os.getenv('URL_PUB')+":"+os.getenv('PORT')+download_link,
-                "tempo_transcricao": tempo_total,
+                "status": "error",
+                "message": "Ocorreu um erro durante a transcrição do áudio."
             }
         )
 
